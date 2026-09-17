7bcca6cea2fb666f7d3d782572650d7ce4c8edcf
Rvngrr <giangonzalvo@gmail.com>
2026-09-18 04:55:42 +0800

    Enhance palindrome detection and update tests for various cases

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,6 +1,34 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    let arr = string.split("");
+    
+    let hasNonStrChar = arr.some((element) => {
+        return /^[0-9]$/.test(element);
+    })
 
+    if (!hasNonStrChar){
+        let strArr = arr.filter((element) => {
+            return /^[a-zA-Z]$/.test(element);
+        })
+
+
+        let strArrRev = [...strArr];
+            strArrRev.reverse();
+
+
+        if(strArr.join("").toLowerCase() === strArrRev.join("").toLowerCase()){
+            console.log(strArr.join(""), strArrRev.join(""));
+            return true;
+        }
+        
+        console.log(strArr.join(""), strArrRev.join(""));
+
+    }
+
+    
+    return false
 };
+
+console.log(palindromes('r3ace3car'));
 
 // Do not edit below this line
 module.exports = palindromes;
--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,6 +1,44 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    if(num < 0){
+        return 'OOPS';
+    }
+    
+    let sum = [0];
 
+    for (let i = 0; i < num; i++){
+        if (i === 0){
+            sum.push(1);
+            // console.log(sum);
+        }
+        else{
+            let curr = sum.length-1
+            let prev = sum.length-2
+
+            sum.push(sum[prev] + sum[curr]);
+            // console.log(sum);
+        }
+
+    };
+    // console.log(sum);
+    return sum.at(-1)
+
+    // if(num < 0){
+    //     return 'OOPS';
+    // }
+    
+    // let prev = 0;
+    // let curr = 1;
+
+    // for (let i = 0; i < num; i++){
+    //     let next = prev + curr;
+    //     prev = curr;
+    //     curr = next;
+
+    // };
+    // // console.log(sum);
+    // return next;
 };
+console.log(fibonacci(10));
 
 // Do not edit below this line
 module.exports = fibonacci;
