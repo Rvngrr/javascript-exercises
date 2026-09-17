83cefd118ad2062f72e6c73cb68a7d535c59141d
Rvngrr <giangonzalvo@gmail.com>
2026-09-17 11:59:09 +0800

    Refactor calculator functions and enable all tests in calculator.spec.js

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,26 +1,51 @@
-const add = function() {
-	
+const add = function(num1,num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1,num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	if(arr.length > 0){
+    return arr. reduce((sum, number) => {
+      return sum + number 
+    },0)
+  }
+  else return 0;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  if(arr.length > 0){
+      return arr. reduce((product, number) => {
+        return product * number 
+      },1)
+    }
+    return 0;
 };
 
-const power = function() {
-	
+const power = function(base, exponent) {
+	if (exponent > 0){
+    return base ** exponent;
+  }
+  return 1
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	let arr = [];
+  if (num > 0){
+    for(let i = num; i > 0; i--){
+      arr.push(i);
+    }
+
+    return multiply(arr);
+  }
+
+  return 1;
 };
+
+
+// console.log(add(3,1), subtract(3,1), sum([3,0,2,1]), multiply([ 6, 5, 4, 3, 2, 1 ]),power(0,0), factorial(6))
 
 // Do not edit below this line
 module.exports = {
