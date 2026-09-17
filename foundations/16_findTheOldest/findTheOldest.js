15d490b404e4372454942081019f9329dac948a9
Rvngrr <giangonzalvo@gmail.com>
2026-09-18 05:38:13 +0800

    Refactor findTheOldest function and update tests to ensure all cases are covered

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,6 +1,30 @@
-const findTheOldest = function() {
+const people = [
+      {
+        name: "Carly",
+        yearOfBirth: 2018,
+      },
+      {
+        name: "Ray",
+        yearOfBirth: 1962,
+        yearOfDeath: 2011,
+      },
+      {
+        name: "Jane",
+        yearOfBirth: 1912,
+        yearOfDeath: 1941,
+      },
+    ]
 
+const findTheOldest = function(arr) {
+    const currentYear = new Date().getFullYear();
+
+    let getOldest = arr.sort((a,b)=>{
+        return ((b.yearOfDeath || currentYear )- b.yearOfBirth) - ((a.yearOfDeath || currentYear ) - a.yearOfBirth)
+    });
+    
+    // console.log(getOldest[0].name);
+    return getOldest[0];
 };
-
+// console.log(findTheOldest(people));
 // Do not edit below this line
 module.exports = findTheOldest;
