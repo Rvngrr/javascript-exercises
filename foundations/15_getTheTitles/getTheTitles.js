775cc1366c593177656e0b520c1858822e18cef3
Rvngrr <giangonzalvo@gmail.com>
2026-09-18 05:05:50 +0800

    Remove commented-out book examples and clean up formatting in getTheTitles.js

--- foundations/15_getTheTitles/getTheTitles.js
+++ foundations/15_getTheTitles/getTheTitles.js
@@ -1,6 +1,29 @@
-const getTheTitles = function() {
+// const books = [
+//       {
+//         title: 'Book',
+//         author: 'Name'
+//       },
+//       {
+//         title: 'Book2',
+//         author: 'Name2'
+//       }
+//     ]
 
+
+const getTheTitles = function(arr) {
+    let books= [];
+
+    for(let i = 0; i < arr.length;i++){
+        books.push(arr[i].title);
+    }
+
+    // return arr.map(book => book.title);
+    return books;
 };
+
+
+
+// console.log(getTheTitles(books))
 
 // Do not edit below this line
 module.exports = getTheTitles;
